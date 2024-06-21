de690bac08ca3b4d4855c467c6d409575f019c27
Adolfo Galué <132372341+Adolfo1996@users.noreply.github.com>
2024-06-21 01:20:47 +0000

    generadordedominio

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  let pronombre = ["El", "Ella"];
+  let pronombre = ["El", "La"];
   let adjetivo = ["Grande", "Gordo"];
-  let sustantivo = ["Cazador", "Oso"];
+  let sustantivo = ["Cazador", "Oso","Bombero"];
   for (let prono = 0; prono < pronombre.length; prono++) {
     for (let adje = 0; adje < adjetivo.length; adje++) {
       for (let sust = 0; sust < sustantivo.length; sust++) {
