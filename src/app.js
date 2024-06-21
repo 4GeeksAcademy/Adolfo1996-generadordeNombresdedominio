b4b1438a9e19e3098a0fcd9a95b92676e96f853f
Adolfo Galué <132372341+Adolfo1996@users.noreply.github.com>
2024-06-21 01:18:07 +0000

    generadorde excusa+

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let pronombre = ["El", "Ella"];
+  let adjetivo = ["Grande", "Gordo"];
+  let sustantivo = ["Cazador", "Oso"];
+  for (let prono = 0; prono < pronombre.length; prono++) {
+    for (let adje = 0; adje < adjetivo.length; adje++) {
+      for (let sust = 0; sust < sustantivo.length; sust++) {
+        console.log(
+          pronombre[adje] + adjetivo[sust] + sustantivo[prono] + ".com"
+        );
+      }
+    }
+  } //write your code here
 };
